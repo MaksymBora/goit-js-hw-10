b6398a8a66e4fe6f9c2c365de93be960c31f736e
MaksymBora <123206568+MaksymBora@users.noreply.github.com>
2023-06-18 18:47:42 +0000

    Deploying to gh-pages from @ MaksymBora/goit-js-hw-10@a4e8f1074f464ad909b65a914d8ba539e64ec13c 🚀

---
+++ index.7c454e6f.js
@@ -0,0 +1,2 @@
+window.addEventListener("scroll",(r=>{document.body.style.cssText+=`--scrollTop: ${window.scrollY}px`})),gsap.registerPlugin(ScrollTrigger,ScrollSmoother),ScrollSmoother.create({wrapper:".wrapper",content:".content"});
+//# sourceMappingURL=index.7c454e6f.js.map
